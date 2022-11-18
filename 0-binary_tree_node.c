b6be15608cbf9614559a87905a21a74f36d9abd7
abel-solo <abel.solo2011@yahoo.com>
2022-11-18 08:08:57 -0800

    first binarh tree task

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,24 +1,24 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - Create a binary node
- * @parent: node parent
- * @value: Value of the node
- * Return: the new node
+ * binary_tree_node - creates a binary tree node
+ * @parent: parent node
+ * @value: node value
+ * Return: new node
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *newNode;
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL)
-	{
+	newNode = malloc(sizeof(binary_tree_t));
+
+	if (!newNode)
 		return (NULL);
-	}
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	return (new_node);
+
+	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	newNode->n = value;
+
+	return (newNode);
 }
